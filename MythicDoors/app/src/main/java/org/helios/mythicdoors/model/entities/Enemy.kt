161b098978e8700846ded44ada799d95fe771b10
Daz <dzafrad@uoc.edu>
2023-11-02 08:45:16 +0100

    Se añade Clase enemy con sus funciones

--- MythicDoors/app/src/main/java/org/helios/mythicdoors/model/entities/Enemy.kt
+++ MythicDoors/app/src/main/java/org/helios/mythicdoors/model/entities/Enemy.kt
@@ -1,6 +1,11 @@
 package org.helios.mythicdoors.model.entities
 
 data class Enemy(
-    val id: Long?
+    val id: Long?,
+    val name: String,
+    val lvl: Int,
+    val coinReward: Int,
+    val image: String
+
     // TODO: Add fields
 ) {}
