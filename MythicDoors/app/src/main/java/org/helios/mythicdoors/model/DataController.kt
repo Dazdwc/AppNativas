1a1155dd5df1495c966da2996612332655976b7b
Daniel Boj <dboj@uoc.edu>
2023-11-03 10:25:45 +0100

    Update Commit -> v1.0 alpha. Añadida lógica de usuario a Data Controller. Añadido patrón singletton en el Data Controler. Añadido patrón Fabric en Enemy. Gradle actualizado a 8.4.

--- MythicDoors/app/src/main/java/org/helios/mythicdoors/model/DataController.kt
+++ MythicDoors/app/src/main/java/org/helios/mythicdoors/model/DataController.kt
@@ -1,4 +1,44 @@
 package org.helios.mythicdoors.model
 
-class DataController {
+import org.helios.mythicdoors.model.entities.User
+import org.helios.mythicdoors.services.EnemyServiceImp
+import org.helios.mythicdoors.services.UserServiceImp
+import org.helios.mythicdoors.services.interfaces.IEnemyService
+import org.helios.mythicdoors.services.interfaces.IUserService
+import java.sql.Connection
+
+class DataController(
+    dbHelper: org.helios.mythicdoors.utils.Connection
+) {
+    private val userService: IUserService = UserServiceImp(dbHelper)
+    /*
+     * Aplicamos un patrón Singgleton para crear el controlador de datos.
+     */
+    companion object {
+        private var instance: DataController? = null
+        fun getInstance(dbHelper: org.helios.mythicdoors.utils.Connection): DataController {
+            if (instance == null) {
+                instance = DataController(dbHelper)
+            }
+            return instance!!
+        }
+    }
+
+    fun getUserService(): IUserService {
+        return userService
+    }
+
+    suspend fun getAllUsers(): List<User>? { return userService.getUsers() }
+
+    suspend fun getUser(id: Long): User? { return userService.getUser(id = id) }
+
+    suspend fun getLastUser(): User? { return userService.getLastUser() }
+
+    suspend fun saveUser(user: User): Boolean { return userService.saveUser(user) }
+
+    suspend fun deleteUser(id: Long): Boolean { return userService.deleteUser(id) }
+
+    suspend fun countUsers(): Int { return userService.countUsers() }
+
+
 }
--- MythicDoors/app/src/main/java/org/helios/mythicdoors/model/entities/Enemy.kt
+++ MythicDoors/app/src/main/java/org/helios/mythicdoors/model/entities/Enemy.kt
@@ -1,6 +1,35 @@
 package org.helios.mythicdoors.model.entities
 
 data class Enemy(
-    val id: Long?
+    private val id: Long?,
+    private val name: String,
+    private val level: Int,
+    private val coinReward: Int,
+    private var image: String?
     // TODO: Add fields
-) {}
+) {
+    fun getId(): Long? { return id }
+    fun getName(): String { return name }
+    fun getLevel(): Int { return level }
+    fun getCoinReward(): Int { return coinReward }
+    fun getImage(): String? { return image }
+
+    fun setImage(image: String) { this.image = image }
+
+    /*
+    * Implementación del patrón Fabric
+    */
+    companion object {
+        fun create(
+            name: String,
+            level: Int,
+            coinReward: Int,
+            ): Enemy {
+            return Enemy(null, name, level, coinReward, null)
+        }
+    }
+
+    fun isEmpty(): Boolean {
+        return id == null
+    }
+}
