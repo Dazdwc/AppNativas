73d7e0bf07e8f33cdf17d72656b2c872db7a7dc9
marcplch <115920853+marcplch@users.noreply.github.com>
2023-11-06 19:14:08 +0100

    Logica juego version 2.0 Marc

--- MythicDoors/app/src/main/java/org/helios/mythicdoors/viewmodel/gamelogic/GameLogicViewModelMarc.kt
+++ MythicDoors/app/src/main/java/org/helios/mythicdoors/viewmodel/gamelogic/GameLogicViewModelMarc.kt
@@ -5,7 +5,38 @@ import org.helios.mythicdoors.model.entities.User
 import kotlin.random.Random
 
 class GameLogicViewModelMarc {
-    fun doorLow(user: User, enemy: Enemy): Int? {
+
+    var lvlRandom: Int? = null
+    val bet = 200 //Con click
+    var win: Int? = null
+    enum class Level {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
+    fun doorLevel(user: User, enemy: Enemy, level: Level): Int? {//Con click le damos el valor a level según la puerta
+        //level = Level.HIGH
+        val lvlRandom = when (level) {
+            Level.LOW -> Random.nextInt(-3, 2)
+            Level.MEDIUM -> Random.nextInt(-1, 4)
+            Level.HIGH -> Random.nextInt(1, 6)
+        }
+        val lvlEnemy = user.getLevel() + lvlRandom
+        val bet = 200
+        var win: Int? = null
+
+        if (lvlEnemy < user.getLevel()) {
+            win = (bet * enemy.getCoinReward())
+            println("Has ganado $win monedas!")
+        } else {
+            win = 0
+            println("Has perdido. Mejor suerte la próxima vez.")
+        }
+
+        return win
+    }
+    /*fun doorLow(user: User, enemy: Enemy): Int? {
         val lvlRandom = Random.nextInt(-3, 2)
         val lvlEnemy = user.getLevel() + lvlRandom
         val bet = 200
@@ -54,6 +85,6 @@ class GameLogicViewModelMarc {
         }
 
         return win
-    }
+    }*/
 
 }
