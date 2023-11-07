08947909c71f4994e56c631059b9226f954722d8
Daz <dzafrad@uoc.edu>
2023-11-07 17:12:42 +0100

    Se hace logica del main

--- MythicDoors/app/src/main/java/org/helios/mythicdoors/ui/screens/LoginScreen.kt
+++ MythicDoors/app/src/main/java/org/helios/mythicdoors/ui/screens/LoginScreen.kt
@@ -1,21 +1,40 @@
 package org.helios.mythicdoors.ui.screens
 
 import androidx.compose.foundation.layout.*
+//import androidx.compose.foundation.layout.ColumnScopeInstance.align
+import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import org.helios.mythicdoors.MainActivity
+import org.helios.mythicdoors.viewmodel.OverviewScreenViewModel
 
 @Composable
 fun LoginScreen(navController: NavController) {
-    Surface(modifier = Modifier
-        .fillMaxSize(),
+
+
+    var username by remember { mutableStateOf("Username") }
+    var password by remember { mutableStateOf("Password") }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
         Text(
@@ -27,33 +46,71 @@ fun LoginScreen(navController: NavController) {
                 .fillMaxWidth()
                 .wrapContentWidth(Alignment.CenterHorizontally),
         )
-        BoxWithConstraints(modifier = Modifier
-            .fillMaxSize(),
+        BoxWithConstraints(
+            modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            Column(modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 30.dp),
-                horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(text = "Login",
+            Column(
+                modifier = Modifier.fillMaxWidth().padding(top = 30.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Login",
                     style = MaterialTheme.typography.headlineMedium,
                     color = MaterialTheme.colorScheme.onBackground,
-                    modifier = Modifier.padding(bottom = 30.dp))
-                Text(text = "Username",
+                    modifier = Modifier.padding(bottom = 30.dp)
+                )
+                Text(
+                    text = "Username",
                     style = MaterialTheme.typography.bodyMedium,
                     color = MaterialTheme.colorScheme.onBackground,
-                    modifier = Modifier.padding(bottom = 10.dp))
-                Text(text = "Password",
+                    modifier = Modifier.padding(bottom = 10.dp)
+                )
+                TextField(
+                    value = username,
+                    onValueChange = { username = it }
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "Password",
                     style = MaterialTheme.typography.bodyMedium,
                     color = MaterialTheme.colorScheme.onBackground,
-                    modifier = Modifier.padding(bottom = 10.dp))
+                    modifier = Modifier.padding(bottom = 10.dp)
+                )
+                TextField(
+                    value = password,
+                    onValueChange = { password = it }
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(top = 15.dp, end = 16.dp),
+                    horizontalArrangement = Arrangement.End,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    FloatingActionButton(
+                        modifier = Modifier.
+                        sizeIn(minWidth = 70.dp, minHeight = 50.dp),
+                        onClick = {
+                            LoginSend(username,password)
+                            }
+                    ) {
+                        Text(text = "Enviar")
+
+                    }
+                }
             }
         }
     }
 }
 
+
 @Preview(showBackground = true)
 @Composable
 fun LoginScreenPreview() {
     LoginScreen(navController = NavController(LocalContext.current))
+}
+
+fun LoginSend(username: String, password: String){
+
+    print(username + password)
+
 }
