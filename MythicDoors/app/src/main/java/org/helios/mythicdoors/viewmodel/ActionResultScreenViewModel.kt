c77d9c308d1cefe6482345a39b5468346f39963d
jdiazrodrigu <129086688+jdiazrodrigu@users.noreply.github.com>
2023-11-09 10:27:13 +0100

    Cambios en lógica de Navegacion

--- MythicDoors/app/src/main/java/org/helios/mythicdoors/viewmodel/ActionResultScreenViewModel.kt
+++ MythicDoors/app/src/main/java/org/helios/mythicdoors/viewmodel/ActionResultScreenViewModel.kt
@@ -1,8 +1,42 @@
 package org.helios.mythicdoors.viewmodel
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import org.helios.mythicdoors.model.DataController
 
 class ActionResultScreenViewModel(
     private val dataController: DataController
-    ): ViewModel() { }
+    ): ViewModel() {
+    fun navigateToGameOptsScreen(navController: NavController,
+                                 scope: CoroutineScope,
+                                 snackbarHostState: SnackbarHostState
+    ) {
+        try {
+            navController.navigate("game_opts_screen")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            scope.launch {
+                snackbarHostState.showSnackbar("Error: Impossible to navigate to Game Option")
+            }
+        }
+    }
+
+    fun navigateToGameActionScreen(navController: NavController,
+                                   scope: CoroutineScope,
+                                   snackbarHostState: SnackbarHostState
+    ) {
+        try {
+            navController.navigate("game_action_screen")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            scope.launch {
+                snackbarHostState.showSnackbar("Error: Impossible to navigate to Game Action")
+            }
+        }
+    }
+    
+    }
+
