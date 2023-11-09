ac006eb8422193706b0e9226a95a14e1a5d63002
jdiazrodrigu <129086688+jdiazrodrigu@users.noreply.github.com>
2023-11-09 17:21:44 +0100

    Cambios en lógica de Navegacion

--- MythicDoors/app/src/main/java/org/helios/mythicdoors/viewmodel/RegisterScreenViewModel.kt
+++ MythicDoors/app/src/main/java/org/helios/mythicdoors/viewmodel/RegisterScreenViewModel.kt
@@ -3,6 +3,7 @@ package org.helios.mythicdoors.viewmodel
 import androidx.compose.material3.SnackbarHostState
 import androidx.navigation.NavController
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import org.helios.mythicdoors.model.DataController
 import org.helios.mythicdoors.navigation.goToLoginScreen
 
@@ -11,7 +12,7 @@ class RegisterScreenViewModel(
     private val dataController: DataController
 ) {
     fun navigateToLoginScreen(navController: NavController, scope: CoroutineScope, snackbarHostState: SnackbarHostState) {
-        goToLoginScreen(navController, scope, snackbarHostState)
+            goToLoginScreen(navController, scope, snackbarHostState)
     }
 
 }
