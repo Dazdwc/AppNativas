e8272b53aba25a70e72b36f4472e1369d45d2eb3
jdiazrodrigu <129086688+jdiazrodrigu@users.noreply.github.com>
2023-11-11 10:00:25 +0100

    Cambios en lógica de Navegacion

--- MythicDoors/app/src/main/java/org/helios/mythicdoors/ui/screens/ScoresScreen.kt
+++ MythicDoors/app/src/main/java/org/helios/mythicdoors/ui/screens/ScoresScreen.kt
@@ -1,7 +1,70 @@
 package org.helios.mythicdoors.ui.screens
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import org.helios.mythicdoors.MainActivity
+import org.helios.mythicdoors.R
+import org.helios.mythicdoors.viewmodel.ScoresScreenViewModel
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.graphics.Color
+
+
 
 @Composable
-fun ScoresScreen(navController: NavController) {}
+fun ScoresScreen(navController: NavController) {
+ //   val controller: ScoresScreenViewModel =
+ //       MainActivity.viewModelsMap["scores-screen-viewmodel"] as ScoresScreenViewModel
+ //   val scope = rememberCoroutineScope()
+ //   val snackbarHostState = remember { SnackbarHostState() }
+
+    Surface(
+        modifier = Modifier,
+
+        color = Color.DarkGray
+    ){ Column(
+        modifier = Modifier
+            .size(300.dp)
+            .padding(30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {Box(
+        modifier = Modifier
+            .background(Color.White)
+            .width(300.dp)
+            .height(500.dp)
+        ){Text(
+        text = "Aquí va el Score")
+            }
+        Spacer(modifier = Modifier.height(35.dp))
+        Button(onClick = {
+         //   controller.navigateToOptsScreen(navController, scope, snackbarHostState)
+        }) {Text(text = "Main Menu")
+        }
+
+    }
+}
+
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun PreviewScoresScreen(){
+    ScoresScreen(navController = NavController(LocalContext.current))
+
+}
